3389f91a9df9f62b20aaaf3e5bd8f02e7bb88fec
Gabriela Verissimo <61754721+GabrielaVerissimo@users.noreply.github.com>
2020-06-16 10:55:10 -0300

    Add files via upload

---
+++ P0030481921038/P0030481921038/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace P0030481921038
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbLista = new System.Windows.Forms.ListBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbLista
+            // 
+            this.lbLista.FormattingEnabled = true;
+            this.lbLista.Location = new System.Drawing.Point(12, 12);
+            this.lbLista.Name = "lbLista";
+            this.lbLista.Size = new System.Drawing.Size(227, 316);
+            this.lbLista.TabIndex = 0;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(52, 350);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(154, 60);
+            this.btnVerificar.TabIndex = 1;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 422);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lbLista);
+            this.Name = "Form1";
+            this.Text = "Prova";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbLista;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ P0030481921038/P0030481921038/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+namespace P0030481921038
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            string valor = "";
+
+
+            valor = Interaction.InputBox("Insira seu último número do RA", "Insira os dados");
+            if (!int.TryParse(valor, out n))
+                MessageBox.Show("Número não válido!");
+            if (n > 9 || n < 0)
+                MessageBox.Show("Número não válido! Insira um número entre 0 e 9.");
+            else if (n == 0)
+                n = 10;
+            else
+            {
+                double[,] tabVend = new double[n, 8];
+                string valTab = "";
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int x = 0; x < 4; x++)
+                    {
+                        valTab = Interaction.InputBox("Insira o valor das vendas do mês " + (i + 1) + ", semana " + (x + 1), "Insira os dados");
+                        if (!double.TryParse(valTab, out tabVend[i, x]))
+                        {
+                            MessageBox.Show("Número não válido!");
+                            x--;
+                        }
+                    }
+                }
+                double totSem, totMes, totGeral, acumulador = 0, acumuladorMes = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int x = 0; x < 4; x++)
+                    {
+                        totSem = tabVend[i, x];
+                        lbLista.Items.Add("Total do mês " + (i + 1) + " Semana " + (x + 1) + ": " + totSem.ToString("C2"));
+                        acumulador += totSem;
+                    }
+                    totMes = acumulador;
+                    lbLista.Items.Add(">> Total Mês: " + totMes.ToString("C2"));
+                    lbLista.Items.Add("-----------------------");
+                    acumulador = 0;
+                    acumuladorMes += totMes;
+                }
+                totGeral = acumuladorMes;
+                lbLista.Items.Add("Total Geral: " + totGeral.ToString("C2"));
+            }
+
+        }
+    }
+}
+        
+    
+
